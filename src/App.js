7edc7ca55878fa7538145505bb3a2219a6a4c800
kishore S <kishore@kishores-MacBook-Pro.local>
2019-10-12 23:24:29 +0530

    parent component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from 'react';
+import NavBar from './Components/NavBar';
+import Books from './Components/Books';
+import axios from 'axios'
+import Cart from './Components/Cart';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    books: [],
+    cart: [],
+    search: ""
+  }
+
+  componentDidMount() {
+    axios.get('https://my-json-server.typicode.com/kishorecodes/jsonserver/books')
+      .then(response => {
+        console.log(response.books)
+        this.setState({
+          books: response.data
+        })
+      })
+  }
+
+  addToCart = id => {
+    const count = this.state.cart.length
+    if(count < 3){
+    this.setState({
+      cart: [...this.state.cart,id]
+    })
+    
+  } else {
+    alert("Maximum three books allowed per user.Proceed to Checkout")
+  }
 }
 
+  resetCartState = () => {
+    this.setState({
+      cart: []
+    })
+  }
+
+  updateCart = (arr) => {
+    this.setState({
+      cart: arr
+    })
+  }
+  
+  filterBooks = (result) => {
+    this.setState({
+      search: result
+    })
+  }
+
+  render() {
+    return (
+      <Router>
+      <div>
+        <NavBar cart={this.state.cart} filterBooks={this.filterBooks}/>
+        <Route exact path="/" render={() => (<Books book={this.state.books} addToCart={this.addToCart} searched={this.state.search} />)} />
+        <Route path="/cart" render={() => (<Cart cart={this.state.cart} book={this.state.books} updateCart={this.updateCart} resetCartState={this.resetCartState} />)}/>
+      </div>
+      </Router>
+    )
+  }
+}
 export default App;
