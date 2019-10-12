c70debaadb68f80b8cc2d4caf72379b6a010a1f9
kishore S <kishore@kishores-MacBook-Pro.local>
2019-10-13 03:00:15 +0530

    Updates

---
+++ precache-manifest.d6606ebe2194946f9efab7080b3148ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a278f3491cc8fddaa8fff5adf740c7",
+    "url": "/library-app-react/index.html"
+  },
+  {
+    "revision": "4487c4c47f1d6abd9139",
+    "url": "/library-app-react/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "4487c4c47f1d6abd9139",
+    "url": "/library-app-react/static/js/2.f625c737.chunk.js"
+  },
+  {
+    "revision": "bcf00b52edb41485cf68",
+    "url": "/library-app-react/static/js/main.d66a3b97.chunk.js"
+  },
+  {
+    "revision": "d535ee0a6fcaa5779443",
+    "url": "/library-app-react/static/js/runtime-main.faef99db.js"
+  }
+]);
